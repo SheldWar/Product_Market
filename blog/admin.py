6df27aa432df94053c5d7f0f711f4fe9d8d9606e
Ivan Kuzmytskyi <shimada0000003@gmail.com>
2021-02-28 21:48:04 +0300

    add blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from .models import Post, Author, Category, Tag
+
 # Register your models here.
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','created_at','photo','views','category','tags','author')
+    list_display_links = ('id', 'title')
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name','lastname', 'author_status')
+    list_display_links = ('id', 'name','lastname')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title','slug')
+    list_display_links = ('id', 'title')
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title','slug')
+    list_display_links = ('id', 'title')
+
+
+
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
