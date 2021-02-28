2135a33301956386587bb7d5b135ae11fdf4c4cc
Sergio <test@gmail.com>
2021-02-28 20:07:14 +0100

    Merge commit

---
+++ blog/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='author_photos/%Y/%m/%d')
+    author_status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ['name']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug': self.slug})
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['title']
+
+    def get_absolute_url(self):
+        return reverse('tag', kwargs={'slug': self.slug})
+
+class Post(models.Model):
+    slug = models.SlugField(max_length=255, unique=True, verbose_name='Url')
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    author = models.ForeignKey(Author, on_delete=models.PROTECT, related_name='authors')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-created_at']
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'slug': self.slug})
