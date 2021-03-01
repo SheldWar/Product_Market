3a711d2bca581c37c3fc995349b9dcc34f0100a2
Sergio <test@gmail.com>
2021-03-01 22:24:45 +0100

    Fix some bugs

--- shop/views.py
+++ shop/views.py
@@ -13,13 +13,20 @@ class Shop(ListView):
     paginate_by = 12
 
     def get_queryset(self):
+        """Если выбрали категорию, то делаем выборку по категории"""
         if self.kwargs:
-            return Product.objects.filter(category__slug=self.kwargs['slug'])
+            return Product.objects.filter(discount=0, category__slug=self.kwargs['slug'])
         else:
-            return Product.objects.all()
+            return Product.objects.filter(discount=0)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
+
+        # если выбрали категорию, ищем товары со скидкой в категории
+        if self.kwargs:
+            context['discounted_products'] = Product.objects.filter(discount__gt=0, category__slug=self.kwargs['slug'])
+        else:
+            context['discounted_products'] = Product.objects.filter(discount__gt=0)
 
         return context
