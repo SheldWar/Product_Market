d93e76a6dc9fd1dcdb841ec18b6a1b318380c2d6
Sergio <test@gmail.com>
2021-03-04 19:29:36 +0100

    Fix some bugs

--- product_market/urls.py
+++ product_market/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('', include('shop.urls')),
     path('blog/', include('blog.urls')),
+    path('', include('shop.urls')),
 ]
